bb17419d519096c118a937e4924fac962c34ac5f
sahulhamm <sahul.opseazy@gmail.com>
2023-11-11 15:12:25 +0530

    addcommit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Sample JavaScript Code
+
+// Function to greet the user
+function greetUser(name) {
+    console.log("Hello, " + name + "! Welcome to the world of JavaScript.");
+}
+
+// Call the function with a sample name
+greetUser("John");
